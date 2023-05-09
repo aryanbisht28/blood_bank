551fb7a67ea1981a663f7f76ddf169a08412fd4b
aryanbisht28 <aryanbisht282001@gmail.com>
2023-05-09 13:10:42 +0530

    Aryan

---
+++ src/pages/Register/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function index() {
+  return (
+    <div className="register-box">
+      <div class="red-box">
+        <h5>Refer a Donor</h5>
+      </div>
+      <div class="white-box">
+        
+      </div>
+    </div>
+  );
+}
+
+export default index;
