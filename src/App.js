9c385a392d2b859b3b5803be38b3cbd4a1d6924f
aryanbisht28 <aryanbisht282001@gmail.com>
2023-05-09 13:10:48 +0530

    Aryan

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import FindBlood from "./pages/find_blood/index";
+import Donor from "./pages/donors/index";
 
 function App() {
   return (
     <div className="App">
-      <FindBlood />
+      {/* <FindBlood /> */}
+      <Donor />
     </div>
   );
 }
